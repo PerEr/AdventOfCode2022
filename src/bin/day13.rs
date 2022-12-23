d6e9157cd69fd0ce3c6ec8d5daf95fe4ac99b234
Per Ersson <per.ersson@gmail.com>
2022-12-23 16:02:01 +0100

    Day 13, part2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::{fs, cmp::Ordering};
 
 use nom::{
     branch::alt,
@@ -7,7 +7,7 @@ use nom::{
     IResult, combinator::{map_res}, sequence::delimited, multi::{separated_list0},
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Item {
     Int(i32),
     List(Vec<Item>),
@@ -37,7 +37,7 @@ fn parse_indata(indata: &str) -> Vec<Vec<Item>> {
         t.split("\n").map(|l| {
             match parse_list(l) {
                 Ok(r) => r.1,
-                Err(_n) => panic!("cant happen"),
+                Err(_n) => panic!("Should not happen"),
             }
     }).collect()
     }).collect()
@@ -75,7 +75,7 @@ fn check_list_order(list0: &Item, list1: &Item) -> Option<bool> {
             }
         }
     } else {
-        panic!("Ermagerd");
+        panic!("Should not happen");
     }
 }
 
@@ -91,10 +91,51 @@ fn add_indicies(lst: Vec<bool>) -> i32 {
     sum
 }
 
+fn mul_delim_indicies(delims: &Vec<Item>, res: &Vec<Item>) -> i32 {
+    let mut index = 0;
+    let mut indicies: Vec<i32> = Vec::new();
+    for ii in res {
+        index += 1;
+        if delims.contains(&ii) {
+            indicies.push(index);
+        }
+    }
+    indicies.into_iter().product()
+}
+
+fn item_cmp(a: &Item, b: &Item) -> Ordering {
+    match check_list_order(a, b) {
+        Some(b) => if b { Ordering::Less } else { Ordering::Greater },
+        None => Ordering::Equal,
+    }
+}
+
+fn append_and_sort(lists: Vec<&str>, res: &mut Vec<Item>) -> Vec<Item> {
+    let mut delims: Vec<Item> = Vec::new();
+    for l in lists {
+        match parse_list(l) {
+            Ok((_, item)) => {
+                delims.push(item.clone());
+                res.push(item)
+            },
+            _ => panic!("Should not happen"),
+        }
+    }
+    res.sort_by(item_cmp);
+    delims
+}
+
 fn main() {
     let indata = fs::read_to_string("data/day13.txt").expect("No indata");
-    let res: Vec<bool> = parse_indata(&indata).into_iter().map(|p| check_list_order(&p[0], &p[1])).map(|o| o.unwrap()).collect();
-    println!("{:?}", add_indicies(res));
+    {
+        let res: Vec<bool> = parse_indata(&indata).into_iter().map(|p| check_list_order(&p[0], &p[1])).map(|o| o.unwrap()).collect();
+        println!("Part 1: {:?}", add_indicies(res));
+    }
+    {
+        let mut res: Vec<Item> = parse_indata(&indata).into_iter().flatten().collect();
+        let delims: Vec<Item> = append_and_sort(vec!("[[2]]", "[[6]]"), &mut res);
+        println!("Part 2: {}", mul_delim_indicies(&delims, &res));
+    }
 
 }
 
@@ -139,5 +180,20 @@ mod tests {
         assert_eq!(13, add_indicies(res));
     }
 
+    #[test]
+    fn test_part2() {
+        let mut res: Vec<Item> = parse_indata(&TEST_DATA).into_iter().flatten().collect();
+        let delims: Vec<Item> = append_and_sort(vec!("[[2]]", "[[6]]"), &mut res);
+        let mut index = 0;
+        let mut indicies: Vec<i32> = Vec::new();
+        for ii in &res {
+            index += 1;
+            if delims.contains(&ii) {
+                indicies.push(index);
+            }
+        }
+        assert_eq!(140, mul_delim_indicies(&delims, &res));
+
+    }
 
 }
