3216ae4eb2cbfa2abf14f3ef77865dd63bcbcc74
Per Ersson <per.ersson@gmail.com>
2022-12-23 17:14:17 +0100

    Draw cave

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,11 +1,17 @@
-use std::{fs};
+use std::{fs, collections::HashMap};
 
 use nom::{
     bytes::complete::tag,
     character::complete::{digit1},
-    IResult, combinator::{map_res}, sequence::{tuple}, multi::{separated_list0, many0},
+    IResult, combinator::{map_res}, sequence::{tuple}, multi::{separated_list0, many0}, And,
 };
 
+#[derive(Debug, PartialEq)]
+enum Content {
+    Rock,
+    Falling,
+    Settled,
+}
 fn parse_integer(input: &str) -> IResult<&str, i32> {
     map_res(digit1, |s: &str| s.parse())(input)
 }
@@ -32,31 +38,85 @@ fn parse_indata(input: &str) -> IResult<&str, Vec<Vec<(i32, i32)>>> {
     }
 }
 
+
+fn build_cave(lst: &Vec<Vec<(i32,i32)>>) -> HashMap<(i32,i32), Content> {
+    let mut cave = HashMap::new();
+    for l in lst {
+        for ss in l.windows(2) {
+            let mut x = ss[0].0;
+            let mut y = ss[0].1;
+            let dx = (ss[1].0 - x).signum();
+            let dy = (ss[1].1 - y).signum();
+            assert!(dx == 0 || dy == 0);
+            loop {
+                cave.insert((x,y), Content::Rock);
+                if x == ss[1].0 && y == ss[1].1 {
+                    break;
+                }
+                x += dx;
+                y += dy;
+            }
+        }
+    }
+    cave
+}
+
+fn draw_cave(cave: &HashMap<(i32,i32), Content>) -> Vec<String> {
+    let mut res = Vec::new();
+    let mut topLeft = (500, 0);
+    let mut bottomRight = (500, 0);
+    for p in cave.keys() {
+        topLeft = (topLeft.0.min(p.0), topLeft.1.min(p.1));
+        bottomRight = (bottomRight.0.max(p.0), bottomRight.1.max(p.1));
+    }
+
+    println!("topleft     {:?}", topLeft);
+    println!("bottomRight {:?}", bottomRight);
+    
+    for y in topLeft.1..=bottomRight.1 {
+        let mut line = String::from("");
+        for x in topLeft.0..=bottomRight.0 {
+            line.push(match cave.get(&(x,y)) {
+                Some(i) if *i == Content::Rock => '#',
+                _ => ' ',
+            });
+        }
+        res.push(line);
+    }
+    res
+}
+
+
+
+
 fn main() {
     let indata = fs::read_to_string("data/day14.txt").expect("No indata");
-    let lst = parse_indata(&indata).ok().unwrap().1;
-    println!("{}", lst.len());
+    let res = parse_indata(&indata).ok().unwrap().1;
+    assert_eq!(179, res.len());
+
+    let cave = build_cave(&res);
+    for l in draw_cave(&cave) {
+        println!("{:?}", l);
+    }
 }
 
 // https://github.com/tumdum/aoc2022/blob/main/src/day12.rs
 #[cfg(test)]
 mod tests {
     use super::*;
-    use indoc::indoc;
 
-    const TEST_DATA: &'static str = indoc! {r#"
-    498,4 -> 498,6 -> 496,6
-    503,4 -> 502,4 -> 502,9 -> 494,9
-    "#};
+    const TEST_DATA: &'static str = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9\n";
 
     #[test]
     fn test_part1() {
-        let res = parse_indata(&TEST_DATA);
-        let lst = res.ok().unwrap().1;
-        assert_eq!(2, lst.len());
-        assert_eq!(vec!((498,4),(498,6),(496,6)), lst[0]);
-        assert_eq!(vec!((503,4),(502,4),(502,9),(494,9)), lst[1]);
+        let res = parse_indata(&TEST_DATA).ok().unwrap().1;
+        assert_eq!(2, res.len());
+        assert_eq!(vec!((498,4),(498,6),(496,6)), res[0]);
+        assert_eq!(vec!((503,4),(502,4),(502,9),(494,9)), res[1]);
+
+        let cave = build_cave(&res);
+        assert_eq!(20, cave.iter().count());
+
+        draw_cave(&cave);
     }
-
-
 }
